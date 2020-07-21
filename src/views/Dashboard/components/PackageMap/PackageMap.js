e205be7cf2991c4af7adac6fe3012455f4d54a30
Zhanzhi <hnzjzz@gmail.com>
2020-07-21 13:40:57 -0500

    add map

--- src/views/Dashboard/components/PackageMap/PackageMap.js
+++ src/views/Dashboard/components/PackageMap/PackageMap.js
@@ -3,64 +3,26 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 
-import {
-  Card,
-  CardHeader,
-  CardContent,
-  CardActions,
-  Divider,
-  Button
-} from '@material-ui/core';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-
-import { data, options } from './chart';
 import GMap from './GMap'
-
 
 const useStyles = makeStyles(() => ({
   root: {},
-
+  chartContainer: {
+    height: 400,
+    position: 'relative'
+  },
+  actions: {
+    justifyContent: 'flex-end'
+  }
 }));
 
 const PackageMap = props => {
 
+  const classes = useStyles();
   return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      {/* <CardHeader
-        action={
-          <Button
-            size="small"
-            variant="text"
-          >
-            Last 7 days <ArrowDropDownIcon />
-          </Button>
-        }
-        title="Latest Sales"
-      />
-      <Divider />*/}
-
-      <CardContent>
-        <div className={classes.chartContainer}>
-          {/* <img alt="map" src="/images/map.jpg"/> */}
-          <GMap></GMap>
-        </div>
-      </CardContent>
-
-      {/*<Divider />
-      <CardActions className={classes.actions}>
-        <Button
-          color="primary"
-          size="small"
-          variant="text"
-        >
-          Overview <ArrowRightIcon />
-        </Button>
-      </CardActions> */}
-    </Card>
+    <div className={classes.chartContainer}>
+      <GMap />
+    </div>
   );
 };
 
