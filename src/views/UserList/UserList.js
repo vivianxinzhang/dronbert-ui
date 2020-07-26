7246df80a66cc93ef75ddb72df7cf57046e7b5ea
Vivian Zhang <vivianxinzhang@gmail.com>
2020-07-26 13:06:43 -0700

    update orderdetail UI and data fetch

--- src/views/UserList/UserList.js
+++ src/views/UserList/UserList.js
@@ -18,23 +18,7 @@ const useStyles = makeStyles(theme => ({
 const UserList = () => {
   const classes = useStyles();
   const [users] = useState(mockData);
-  const [orderDetail, setOrderDetail] = useState({
-    orderNumber: 23432423,
-    senderFirstName: 'Jack',
-    senderLastName: 'Chan',
-    senderAddress: '1600 Amphitheatre Parkway, Mountain View, CA, 94043',
-    senderPhoneNumber: '1111111111',
-    senderEmail: 'jack@gmail.com',
-    recipientFirstName: 'Jeff',
-    recipientLastName: 'Car',
-    recipientAddress: '1 Hacker Way, Menlo Park, CA, 94025',
-    recipientPhoneNumber: '2222222222',
-    recipientEmail: 'jeff@gmail.com',
-    packageWeight : '10',
-    packageHeight : '10',
-    packageLength : '10',
-    packageWidth : '10',
-  });
+  const [orderDetail, setOrderDetail] = useState([]);
 
   // transfer order number between sibling component
   // const [ orderNumber, setOrderNumber ] = useState(mockData[0].orderID);
@@ -75,6 +59,7 @@ const UserList = () => {
       .then(response => {
         // console.log(response.data);
         setOrderHistory(response.data);
+        getOrderDetail();
       })
       .catch(error => console.log(error));
   }
--- src/views/UserList/components/OrderDetail/OrderDetail.js
+++ src/views/UserList/components/OrderDetail/OrderDetail.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  Table,
+  TableCell,
+  IconButton,
+  Divider,
+  Typography
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    height: 90,
+    height: 150,
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
   detailInfo: {
-    fontSize: 30
+    fontSize: 20
   }
 }));
 
@@ -26,19 +36,75 @@ const OrderDetail = (props) => {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            {/*Order Detail: {orderDetail['orderNumber']}*/}
-            <div className={classes.detailInfo}>
-              Order Detail:
-              <br />
-              The current selected order is #{ orderNumber }
-              Name: {orderDetail['sender_name']}     Phone: {orderDetail['sender_phone']}    Email: {orderDetail['sender_email']}
-            </div>
-          </Paper>
+      <Card>
+        <CardHeader
+          title="Order Details"
+        />
+        <CardContent>
+          <div className={classes.chartContainer}>
+            <Table >
+              <TableCell>
+
+              </TableCell>
+            </Table>
+            #{ orderNumber }  Total Cost: ${ orderDetail['total cost'] }
+          </div>
+        </CardContent>
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          spacing={0}>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>Sender Info:
+              <br/>
+              <br/>
+              Name: {orderDetail['sender_name']}
+              <br/>
+              Phone: {orderDetail['sender_phone']}
+              <br/>
+              Email: {orderDetail['sender_email']}
+              <br/>
+              Address: {orderDetail['sender_address']}
+            </Paper>
+            {/*<Paper className={classes.paper}>Order Detail*/}
+            {/*  <br/>*/}
+            {/*  <br/>*/}
+            {/*  Total Cost: { orderDetail['total cost'] }*/}
+            {/*  <br/>*/}
+            {/*  Shipping Option: { orderDetail['machine_type']}*/}
+            {/*  <br/>*/}
+            {/*  Estimated Delivery Time: { orderDetail['delivered_at'] }*/}
+            {/*</Paper>*/}
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>Recipient Info:
+              <br/>
+              <br/>
+              Name: {orderDetail['recipient_name']}
+              <br/>
+              Phone: {orderDetail['recipient_phone']}
+              <br/>
+              Email: {orderDetail['recipient_email']}
+              <br/>
+              Address: {orderDetail['recipient_address']}
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>Package Info:
+              <br/>
+              <br/>
+              Length: {orderDetail['package_length']}
+              <br/>
+              Weight: {orderDetail['package_weight']}
+              <br/>
+              Height: {orderDetail['package_height']}
+              <br/>
+              Fragile:{orderDetail['package_fragile']}
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </Card>
     </div>
   );
 }
